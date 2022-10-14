61a08be0e3c0fd82ce5e8a162e96ef45d5d2a016
Yeon-Moo <zhang0ben0@gmail.com>
2022-10-14 23:38:20 +0800

    add Background and gif button

--- first.js
+++ first.js
@@ -1,5 +1,3 @@
-let many = [1,"octopus",[87,69,54]];
-
 $(document).ready(function(){
     $("#button1").click(function(){
         $("#changed").text("Paragraoh changed.");
@@ -8,9 +6,15 @@ $(document).ready(function(){
 
 var i=0;
   function Click (){
-    i++;
+    
+    document.body.style.backgroundImage="url('https://agirls.aottercdn.com/media/0d532b3f-0196-466a-96e1-7c6db56d0142.gif')";
+    
 }
 
 function PressMe(){
     document.getElementById("changed").innerHTML="HAHAHA";
+}
+
+var BackGround=function(){
+    document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1665713800863-16a4f96c3a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=397&q=80')";
 }
