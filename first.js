d4ca93c12ef961aa25fbf5c2fb08870a9e29ca4b
Yeon-Moo <zhang0ben0@gmail.com>
2022-10-14 21:01:23 +0800

    JavaScript

---
+++ first.js
@@ -0,0 +1,15 @@
+let many = [1,"octopus",[87,69,54]];
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#changed").text("Paragraoh changed.");
+    });
+});
+var i=0;
+  function Click (){
+    i++;
+}
+
+function PressMe(){
+    document.getElementById("changed").innerHTML="HAHAHA";
+}
