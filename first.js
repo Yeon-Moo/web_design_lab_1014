b6ced659bd36dd8211470a1afc60c469326ee2e3
Yeon-Moo <zhang0ben0@gmail.com>
2022-10-15 17:04:13 +0800

    add some css file

--- first.js
+++ first.js
@@ -18,3 +18,48 @@ function PressMe(){
 var BackGround=function(){
     document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1665713800863-16a4f96c3a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=397&q=80')";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//object
+/* let person={
+    name:'chang',
+    age:10,
+    gender:"boy",
+    hobby:[2,1,3,true,'a']
+};
+*/
+
+//if statement
+/* var test=true;
+if(test){
+    document.write("hello genius");
+
+} */
+
+//while loop
+
+/* var i=0;
+while(i<5){
+    document.write("hello genius\n");
+    i++;
+} */
+
+
+//for loop and html
+/* var i=0;
+var Link=document.getElementsByTagName("a");
+
+for(i=0;i<Link.length;i++){
+    Link[i].className="Link"+i;
+}
+ */
